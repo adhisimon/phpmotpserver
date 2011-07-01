b11a29186684bf475f57a34d590e8c76d7f8b055
Adhidarma Hadiwinoto <adhisimon@mondial.co.id>
2011-07-01 13:13:38 +0700

    modified core.php

--- htdocs/app/config/core.php
+++ htdocs/app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '1236782163762178');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -302,3 +302,8 @@
  *
  */
 	Cache::config('default', array('engine' => 'File'));
+
+/**
+ * import core.local.php if exists
+ */
+@include('core.local.php');
